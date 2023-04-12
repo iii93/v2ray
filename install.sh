bea30411bc2fa3b9463b5ff5d647bd3f81501c05
iii93 <programlee93@gmail.com>
2023-04-12 22:07:23 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#ws+tls+web
 #切换root用户执行下面命令
 apt update && apt upgrade -y
 apt install vim
@@ -6,7 +7,7 @@ apt install curl
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 systemctl enable v2ray
 #安装caddy
- apt install -y debian-keyring debian-archive-keyring apt-transport-https
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 apt update
