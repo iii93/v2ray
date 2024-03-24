5d02847e52b5ec188644ef4f21e1846ce02e92d5
iii93 <programlee93@gmail.com>
2024-03-24 13:05:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 systemctl enable v2ray
 #安装caddy
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list 
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list 
 apt update
 apt install caddy
 # 开启bbr（添加参数到/etc/sysctl.conf文件）
@@ -15,3 +15,4 @@ echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_window_scaling=1" >> /etc/sysctl.conf
 sysctl -p
+apt autoremove
