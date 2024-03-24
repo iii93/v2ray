bb304aa52c90f7d5864305934d470988bdc4f92f
iii93 <programlee93@gmail.com>
2024-03-24 09:41:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-#ws+tls+web
-#切换root用户执行下面命令
 apt update && apt upgrade -y
 apt install vim curl git -y
 git clone https://github.com/iii93/v2ray.git
@@ -12,22 +10,6 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --d
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list 
 apt update
 apt install caddy
-#修改v2ray和caddy配置文件
-rm /usr/local/etc/v2ray/config.json
-rm /etc/caddy/Caddyfile
-rm /usr/share/caddy/index.html
-mv /v2ray/config.json /usr/local/etc/v2ray/ 
-read -p "请输入你的id（a7f43ece-5d93-46a5-94e8-5a20d19cf3dc）：" uuid  
-sed -i 's/a7f43ece-5d93-46a5-94e8-5a20d19cf3dc/'"${uuid}"'/g' /usr/local/etc/v2ray/config.json 
-read -p "请输入你的路径：" path
-sed -i 's/ray/'"${path}"'/g' /usr/local/etc/v2ray/config.json  
-mv /v2ray/Caddyfile /etc/caddy/ 
-read -p "请输入你的域名：" domain
-sed -i "s/https:\/\/abc\.com/${domain}/g" /etc/caddy/Caddyfile 
-sed -i 's/ray/'"${path}"'/g' /etc/caddy/Caddyfile 
-mv /v2ray/index.html /usr/share/caddy
-systemctl start v2ray
-systemctl start caddy
 # 开启bbr（添加参数到/etc/sysctl.conf文件）
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
